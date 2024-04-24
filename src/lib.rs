90ab990589ac8c8abd303d67b5de97f778d1f471
changerlemond <83348294+changerlemond@users.noreply.github.com>
2024-04-24 19:31:49 +0900

    feature: subscription

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
 use std::net::TcpListener;
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpServer, HttpResponse};
 use actix_web::dev::Server;
 
-async fn health_check() -> impl Responder {
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -10,6 +20,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
         .run();
